6e5f4fce6d7fa700c0415e3e091d2ba1c08b69cd
Hadil24A <hadil.ahmed@hva.nl>
2024-11-19 14:03:40 +0100

    Data opgehaald

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
 <script>
+    export let data;
+    	console.log(data); 
     import Filters from '$lib/Filters.svelte'; 
 </script>
+
+{#each data.artObjects as art}
+    <ul>
+        <li>
+            <h2>{art.title}</h2>
+            <img src={"https://fdnd-agency.directus.app/assets/" + art.image} alt={art.title} width="400px" />
+        </li>
+    </ul>
+{/each}
+
 <Filters />
