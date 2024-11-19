cc5bfebbed44d03d8033ffd92250871ba98deb8c
Hadil24A <hadil.ahmed@hva.nl>
2024-11-19 16:20:30 +0100

    beginnetje functie filter buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,14 @@
     import Filters from '$lib/Filters.svelte'; 
 </script>
 
+<Filters />
+
 {#each data.artObjects as art}
-    <ul>
-        <li>
+    <ul class="masonry">
+        <li class="masonry-item" data-category= "{art.techniques}">
             <h2>{art.title}</h2>
             <img src={"https://fdnd-agency.directus.app/assets/" + art.image} alt={art.title} width="400px" />
         </li>
     </ul>
 {/each}
 
-<Filters />
