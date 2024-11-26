1c3cbbfb7b4696b77a919d2ca1faa4a16ebd9bf4
Hadil24A <hadil.ahmed@hva.nl>
2024-11-27 00:14:34 +0100

    closed #1

--- src/lib/molecules/Filters.svelte
+++ src/lib/molecules/Filters.svelte
@@ -1,38 +1,48 @@
-<input type="radio" id="All objects" name="categories" value="All">
-<input type="radio" id="Pottery" name="categories" value="Pottery">
-<input type="radio" id="Islamic art" name="categories" value="Islamic art">
-<input type="radio" id="Tapestry" name="categories" value="Tapestry">
-<input type="radio" id="Glass" name="categories" value="Glass">
+<script>
+  import { activeFilter } from "$lib/store";
 
-<ul class="filter" >    
+  function setFilter(filter) {
+    activeFilter.set(filter);
+  }
+</script>
 
-    <li><button data-filter="*">All objects</button></li>
-    <li><button data-filter="Pottery">Pottery</button></li>
-    <li><button data-filter="Islamic art">Islamic art</button></li>
-    <li><button data-filter="Tapestry">Tapestry</button></li>
-    <li><button data-filter="Glass">Glass</button></li>
+<ul>
+  <li>
+    <button
+      on:mouseover={() => activeFilter.set("*")}
+      on:mouseleave={() => ($activeFilter = "*")}>All objects</button
+    >
+  </li>
+  <li>
+    <button
+      on:mouseover={() => activeFilter.set("Pottery")}
+      on:mouseleave={() => ($activeFilter = "*")}>Pottery</button
+    >
+  </li>
+  <li>
+    <button
+      on:mouseover={() => activeFilter.set("Islamic art")}
+      on:mouseleave={() => ($activeFilter = "*")}>Islamic art</button
+    >
+  </li>
+  <li>
+    <button
+      on:mouseover={() => activeFilter.set("Tapestry")}
+      on:mouseleave={() => ($activeFilter = "*")}>Tapestry</button
+    >
+  </li>
+  <li>
+    <button
+      on:mouseover={() => activeFilter.set("Glass")}
+      on:mouseleave={() => ($activeFilter = "*")}>Glass</button
+    >
+  </li>
 </ul>
 
 <style>
-        
-    [value="All objects"]:checked .masonry .masonry-item:not([data-category~="All objects"]),
-    [value="Pottery"]:checked .masonry .masonry-item:not([data-category~="Pottery"]),
-    [value="Islamic art"]:checked .masonry .masonry-item:not([data-category~="Islamic art"]),
-    [value="Tapestry"]:checked .masonry .masonry-item:not([data-category~="Tapestry"]),
-    [value="Glass"]:checked .masonry .masonry-item:not([data-category~="Glass"]) 
-    {
-        background-color: rgb(0, 0, 0, 0.7);
-    }
-    
-    input[type="radio"] {
-        position: absolute;
-        left: -999px;
-    }
-
-    .filter {
+  ul {
     bottom: 5em;
     display: flex;
-    justify-content: center;
     margin: 0;
     overflow: scroll;
     padding: 0;
@@ -41,59 +51,59 @@
     transform: translateX(-50%);
     width: 25em;
     z-index: 268;
-}
+  }
 
-    ::-webkit-scrollbar {
-        display: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  ul li {
+    cursor: pointer;
+  }
+
+  @media (prefers-reduced-motion) {
+    ul li button:focus,
+    ul li button:hover {
+      box-shadow: 2px 2px 2px #989802;
+      background-color: #ffff00;
+      color: #000;
     }
+  }
 
-    .filter label, .filter li  {
-        cursor: pointer;
+  ul li {
+    list-style-type: "";
+  }
+
+  ul button {
+    background-color: #464646;
+    border: none;
+    color: white;
+    cursor: pointer;
+    font-size: 18px;
+    margin: 0.5em;
+    padding: 0.5em;
+    width: 80px;
+    text-align: center;
+    list-style-type: "";
+    transition: 0.5s;
+    width: max-content;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    ul button:focus,
+    ul button:hover {
+      box-shadow: inset 6.5em 0 0 0 #ffff00;
     }
+  }
 
-    @media (prefers-reduced-motion) {
-        .filter li button:focus, .filter li button:hover {
-            box-shadow: 2px 2px 2px #989802;
-            background-color: #ffff00;
-            color: #000;
-        }
+  ul button:hover,
+  ul button:focus {
+    color: #000;
+  }
+
+  @media (min-width: 600px) {
+    ul {
+      width: max-content;
     }
-
-    .filter li {
-        list-style-type: "";
-    }
-
-    .filter button {
-        background-color: #464646;
-        border: none;
-        color: white;
-        cursor: pointer;
-        font-size: 18px;
-        margin: 0.5em;
-        padding: 0.5em;
-        width: 80px;
-        text-align: center;
-        list-style-type: "";
-        transition: 0.5s;
-        width: max-content;
-    }
-
-    @media (prefers-reduced-motion: no-preference) {
-        .filter button:focus, .filter button:hover {
-            box-shadow: inset 6.5em 0 0 0 #ffff00;
-        }
-    }
-
-    .filter button:hover, .filter button:focus {
-        color: #000;
-    }
-
-
-
-    @media (min-width: 600px) {
-        .filter {
-            width: max-content;
-        }
-    }
-
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,99 +1,162 @@
 <script>
-    export let data;
-    	console.log(data); 
-    import Filters from '$lib/molecules/Filters.svelte'; 
+  export let data;
+  console.log(data);
+
+  import { activeFilter } from "$lib/store";
+  import Filters from "$lib/molecules/Filters.svelte";
+
+  const techniques = ["Pottery", "Islamic art", "Tapestry", "Glass"];
 </script>
 
 <Filters />
+
 <ul class="masonry">
-    {#each data.artObjects as art}
-    <li class="masonry-item" data-category="{art.techniques}">
-        <figure>
-            <img src={"https://fdnd-agency.directus.app/assets/" + art.image} alt={art.title} />
-            <figcaption>
-                <h2>{art.title}</h2>
-            </figcaption>
-        </figure>
+  {#each data.artObjects as art, index}
+    <li
+      class="masonry-item"
+      class:hidden={$activeFilter !== "*" &&
+        $activeFilter !== techniques[index % techniques.length]}
+      data-category={techniques[index % techniques.length]}
+    >
+      <figure>
+        <picture>
+          <source
+            srcset={"https://fdnd-agency.directus.app/assets/" +
+              art.image +
+              ".avif"}
+            type="image/avif"
+          />
+          <source
+            srcset={"https://fdnd-agency.directus.app/assets/" +
+              art.image +
+              ".webp"}
+            type="image/webp"
+          />
+          <img
+            src={"https://fdnd-agency.directus.app/assets/" + art.image}
+            alt={art.title}
+            height={art.height}
+            width={art.width}
+            loading="lazy"
+          />
+        </picture>
+        <figcaption>
+          <h2>{art.title}</h2>
+        </figcaption>
+      </figure>
     </li>
-    {/each}
+  {/each}
 </ul>
 
 <style>
+  .masonry {
+    column-count: 1;
+    column-gap: 1rem;
+    list-style: none;
+    padding: 0;
+  }
+
+  .masonry-item {
+    break-inside: avoid;
+    display: block;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    margin-bottom: 1rem;
+    position: relative;
+    overflow: hidden;
+    transition: opacity 0.3s;
+  }
+
+  .masonry-item.hidden {
+    filter: opacity(0.3);
+    pointer-events: none;
+    transition: 1s;
+  }
+
+  figure {
+    margin: 0;
+    position: relative;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    display: block;
+    border-radius: 8px;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  figcaption {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .masonry-item:hover img {
+    transform: scale(1.1);
+  }
+
+  .masonry-item:hover figcaption {
+    opacity: 1;
+  }
+
+  h2 {
+    font-size: 16px;
+    margin: 0.5rem 0;
+    text-align: center;
+    -webkit-text-stroke: 0.2px #ffff00;
+  }
+
+  /* Responsieve layout */
+  @media (min-width: 600px) {
     .masonry {
-        column-count: 1;
-        column-gap: 1rem;
-        list-style: none;
-        padding: 0;
+      column-count: 2;
     }
+  }
 
-    .masonry-item {
-        break-inside: avoid;
-        margin-bottom: 1rem;
-        position: relative;
-        overflow: hidden;
-        background-color: #fff;
-        border-radius: 8px;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    }
-
-    figure {
-        margin: 0;
-        position: relative;
-    }
-
-    img {
-        width: 100%;
-        height: auto;
-        display: block;
-        border-radius: 8px;
-        transition: transform 0.3s ease-in-out;
-    }
-
-    figcaption {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.6);
-        color: white;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        opacity: 0;
-        transition: opacity 0.3s ease-in-out;
-    }
-
-    .masonry-item:hover img {
-        transform: scale(1.1);
-    }
-
-    .masonry-item:hover figcaption {
-        opacity: 1;
+  @media (min-width: 900px) {
+    .masonry {
+      column-count: 3;
     }
 
     h2 {
-        font-size: 16px;
-        margin: 0.5rem 0;
-		text-align: center;
-		-webkit-text-stroke: 0.2px #ffff00;
+      font-size: 1.5rem;
+    }
+  }
+
+  @media (prefers-reduced-motion) {
+    img {
+      width: 100%;
+      height: auto;
+      display: block;
+      border-radius: 8px;
     }
 
-    /* Responsieve layout */
-    @media (min-width: 600px) {
-        .masonry {
-            column-count: 2;
-        }
+    .masonry-item {
+      break-inside: avoid;
+      display: block;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      margin-bottom: 1rem;
+      position: relative;
+      overflow: hidden;
     }
 
-    @media (min-width: 900px) {
-        .masonry {
-            column-count: 3;
-        }
-
-        h2 {
-            font-size: 1.5rem;
-        }
+    .masonry-item.hidden {
+      filter: opacity(0.1);
+      pointer-events: none;
     }
+  }
 </style>
